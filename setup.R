36caf2a1d5daa98db0590b2c320e24d1fc95f167
Michael Foley <mpfoley73@hotmail.com>
2023-10-31 05:16:51 -0400

    New book build.

--- setup.R
+++ setup.R
@@ -2,11 +2,12 @@ theme_set(
   theme_light() +
     theme(
       strip.background = element_rect(fill = "gray90"),
-      # text = element_text(family = "Rockwell Condensed", size = 16, color = "#003831"),
+      strip.text = element_text(color = "gray10"),
+      text = element_text(color = "gray10"),
       # plot.subtitle = element_text(family = "Rockwell", size = 12),
       # plot.caption = element_text(size = 10, hjust = 0, color = "#666666"),
       panel.grid.minor = element_blank(),
       legend.position = "top",
-      axis.title.y.right = element_text(margin = margin(0, 0, 0, 10))
+      axis.title.y.right = element_text(margin = margin(0, 0, 0, 10)) #trbl
     )
 )
